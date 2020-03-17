a1cd70e6565c44feecc6c148bc093aa955c39dea
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-03-16 21:42:23 -0300

    Criando interface da aplicação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Main = () => <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong> (Repositório do facebook)
+            </p>
+            <a href="http://github.com/facebook/react">Acessar</a>
+          </li>
+        </ul>
+      </>
+    );
+  }
+}
